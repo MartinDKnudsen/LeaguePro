d1d8e4b45dce6f3e4d8da86456eb126a24ad3efb
MartinDKnudsen <martinknu22@gmail.com>
2020-09-03 22:06:25 +0200

    added item picker

--- App.js
+++ App.js
@@ -1,22 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import WelcomeScreen from "./app/assets/screens/FrontPageScreen";
 import ListingDetailsScreen from "./app/assets/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/assets/screens/MessagesScreen";
 import Screen from "./app/assets/Components/Screen";
 import Icon from "./app/assets/Components/icon";
-import ListCounter from "./app/assets/Components/ListCounters";
 import Messages from "./app/assets/Components/Messages";
-import TestScreen from "./app/assets/screens/TestScreen";
 import AccountScreen from "./app/assets/screens/AccountScreen";
 import ListingChampsScreen from "./app/assets/screens/ListingChampsScreen";
-import { TextInput, Text, Switch } from "react-native";
-import AppTextInput from "./app/assets/Components/AppTextInput";
-import AppPicker from "./app/assets/Components/AppPicker";
-import AppText from "./app/assets/Components/AppText";
+import { TextInput, Text, Switch, Image } from "react-native";
+import AppTextInput from "./app/assets/Components/TextInput";
+import AppPicker from "./app/assets/Components/Picker";
+import AppText from "./app/assets/Components/Text";
 import LoginScreen from "./app/assets/screens/LoginScreen";
-import AppButton from "./app/assets/Components/AppButton";
+import AppButton from "./app/assets/Components/Button";
 import RegisterScreen from './app/assets/screens/RegisterScreen'
+import Button from './app/assets/Components/Button'
+
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+
 
 export default function App() {
-  return <RegisterScreen/>;
+const [imageUri, setImageUri] = useState();
+
+ const requestPermission = async () => {
+  const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+if (!granted) alert("You need to enable permission to access the library.");
+
 }
+
+ useEffect(() => {
+requestPermission();
+ }, [])
+ 
+const selectImage = async () => {
+  try {
+    const result = await ImagePicker.launchImageLibraryAsync();
+   if (!result.cancelled)
+   setImageUri(result.uri);
+  } catch (error) {
+    console.log("Error reading a image", error)
+  }
+}
+
+  return (
+    <Screen>
+      <Button title="Select image" onPress={selectImage}/>
+      <Image source={{uri: imageUri }} style={{width: 400, height: 800}}/>
+    </Screen>
+  );
+
+}
+
